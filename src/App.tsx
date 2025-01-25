d97946e9b6be4b11775bb96b1e2d0e10c19b885a
Martin Lindström <martinllindstroem@gmail.com>
2025-01-25 17:57:35 +0100

    Add paths

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
+import Signup from "./pages/Signup";
 import NoPage from "./pages/NoPage";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="Register" element={<Register />} />
-          <Route path="Login" element={<Login />} />
-          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="login" element={<Login />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="profile" element={<Profile />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
